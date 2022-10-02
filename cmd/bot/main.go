9c3487ae8113bd637022f713523c89206b737086
M1racle-Heen <70756496+M1racle-Heen@users.noreply.github.com>
2022-10-02 21:19:11 +0600

    update

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	bot, err := tgbotapi.NewBotAPI("5797499341:AAEyTUebxUuyDqnqIHrS27DiHjzoq5WK2jI")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -16,17 +17,24 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+	// u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+			j := update.Message.From.UserName + " said " + update.Message.Text
+			ok := strings.Contains(j, "/exit")
+			if ok {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "You have exited chat"))
+				return
+			}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, j)
+			// msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
