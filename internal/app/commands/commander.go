920a3c3ab2b608b5b1a5b944555bcae391b47d44
M1racle-Heen <70756496+M1racle-Heen@users.noreply.github.com>
2022-10-02 23:04:49 +0600

    test

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -6,8 +6,6 @@ import (
 	"github.com/M1racle-Heen/bot/internal/service/product"
 )
 
-var registeredCommands = map[string]func(c *Commander, msg *tgbotapi.Message){}
-
 type Commander struct {
 	bot            *tgbotapi.BotAPI
 	productService *product.Service
@@ -28,4 +26,8 @@ func (c *Commander) HandleUpdate(update tgbotapi.Update) {
 			c.Default(update.Message)
 		}
 	}
+}
+
+func String() string {
+	return ""
 }
